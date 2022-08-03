ad8a9621e92e0b5bf2b827535fbab86e7b80662e
Michael Joel <michaeljoelt@gmail.com>
2022-08-03 13:11:10 -0700

    added some interaction

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const cors = require('cors') //allows requests from external clients
 app.use(cors())
 const PORT = 8000
 
+/*
+for handling query strings
+using insteadhttps://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+*/
+// const url = require('url');
+// const querystring = require('querystring');
+
 const food = {
     "chicken": {
         "internalTempF": 140,
@@ -85,6 +92,35 @@ app.get("/api/:category/:type", (request, response) => {
     // jsonString = JSON.stringify(food, null, 2)
     // console.log(`searching trhough: ${jsonString}`)
 
+
+    if (food[category].types[type]) {
+        response.json(food[category].types[type])
+        console.log(food[category].types[type])
+    } else {
+        response.json(food['unknown'].types['unknown'])
+        console.log(food['unknown'].types['unknown'])
+    }
+})
+
+app.get("/lookup", (request, response) => {
+    // let rawUrl = request.originalUrl;
+    // const url = request.originalUrl
+    // const searchParams = new URLSearchParams(url);
+    // const category = searchParams.get("category")
+    // const type = searchParams.get("type")
+    const category = request.query.category
+    const type = request.query.type
+    console.log(`Found queries: ${category} and ${type}`)
+    // console.log(request)
+
+    // const category = (request.params.category).toLowerCase()
+    // const type = (request.params.type).toLowerCase()
+    // console.log(`searching for Category: ${category}, Type: ${type}`)
+
+    //testing
+    // jsonString = JSON.stringify(food, null, 2)
+    // console.log(`searching trhough: ${jsonString}`)
+
     if (food[category].types[type]) {
         response.json(food[category].types[type])
         console.log(food[category].types[type])
@@ -102,3 +138,5 @@ app.listen(process.env.PORT || PORT, () => { //process.env.PORT allows you to us
     console.log(`listening on port ${PORT}`)
     console.log(`search api using pattern: /api/chicken/breast (replace with category/type, like api/pork/bacon)`)
 })
+
+// if getting error that 8000 is used: npx kill-port 8000
